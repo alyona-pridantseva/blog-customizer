7c2cf9afcb5e5f5cb6979083c45a7e251edd54a7
Alyona Pridantseva <pridantseva.alyona@gmail.com>
2024-10-03 01:03:46 +0300

    container

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,12 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
 	return (
 		<>
 			<ArrowButton />
-			<aside className={clsx(styles.container, styles.container_open)}>
+			<aside className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
