dec55fd4b0a7e5dd869495984994ee02aa1f0fd1
PationPrime <pationprime@gmail.com>
2024-10-03 00:36:06 +0300

    added husky and lint-stagged

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,13 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
 	return (
 		<>
 			<ArrowButton />
-			<aside className={styles.container}>
+			<aside className={clsx(styles.container, styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
